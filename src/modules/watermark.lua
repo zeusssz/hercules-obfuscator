63d71a0dae7a781e8c3e867eb8c3837aa7462187
zeus <136583387+zeusssz@users.noreply.github.com>
2024-08-25 22:39:27 +0530

    Update watermark.lua

--- src/modules/watermark.lua
+++ src/modules/watermark.lua
@@ -2,7 +2,7 @@
 local Watermark = {}
 
 function Watermark.add_watermark(code)
-    return "--[Obfuscated by Shitface]\n" .. code
+    return "--[Obfuscated by Hercules]\n" .. code
 end
 
 return Watermark
