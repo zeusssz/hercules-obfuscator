d8b60582e96405f2b82cdc10cc20975f4c5073e4
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-01 21:49:04 +0530

    Update variable_renamer.lua

--- src/modules/variable_renamer.lua
+++ src/modules/variable_renamer.lua
@@ -1,24 +1,25 @@
--- modules/variable_renamer.lua
-local VariableRenamer = {}
+local StringEncoder = {}
 
-local function generate_random_name(len)
-    local res = ""
-    for i = 1, len do
-        res = res .. string.char(math.random(97, 122)) -- a-z
-    end
-    return res
+local function base64_encode(data)
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    local encoded = ((data:gsub('.', function(x)
+        local r, b = '', x:byte()
+        for i = 8, 1, -1 do r = r .. (b % 2^i - b % 2^(i-1) > 0 and '1' or '0') end
+        return r
+    end) .. '0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if #x < 6 then return '' end
+        local c = 0
+        for i = 1, 6 do c = c + (x:sub(i, i) == '1' and 2^(6 - i) or 0) end
+        return b:sub(c + 1, c + 1)
+    end) .. ({ '', '==', '=' })[#data % 3 + 1])
+    return encoded
 end
 
-function VariableRenamer.process(code)
-    local variables = {}
-    local i = 0
-    return code:gsub("([%a_][%w_]*)", function(var)
-        if not variables[var] then
-            i = i + 1
-            variables[var] = generate_random_name(math.random(8, 12))
-        end
-        return variables[var]
+function StringEncoder.process(code)
+    return code:gsub('"([^"]-)"', function(str)
+        local encoded_str = base64_encode(str)
+        return '"..("' .. encoded_str .. '"):gsub("%w", function(c) return string.char(c:byte() - 2) end).."'
     end)
 end
 
-return VariableRenamer
+return StringEncoder
