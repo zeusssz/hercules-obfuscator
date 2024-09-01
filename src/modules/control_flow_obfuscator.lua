a23d674cebbe3962ca2b53a0b925320af7de7930
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-01 21:44:28 +0530

    Update control_flow_obfuscator.lua

--- src/modules/control_flow_obfuscator.lua
+++ src/modules/control_flow_obfuscator.lua
@@ -2,15 +2,12 @@ local ControlFlowObfuscator = {}
 
 function ControlFlowObfuscator.process(code)
     local function insert_fake_control_flow(original_code)
-        -- Encapsulate the original code in a fake loop and condition
         local obfuscated_code = string.format(
             "while true do if false then break end %s break end",
             original_code
         )
         return obfuscated_code
     end
-
-    -- Process the input code with fake control flow
     local obfuscated_code = insert_fake_control_flow(code)
 
     return obfuscated_code
