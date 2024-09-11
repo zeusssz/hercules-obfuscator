f4b582b1396dcab84de51b320c859085aad50033
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-11 12:50:44 +0530

    Update hercules.lua

--- src/hercules.lua
+++ src/hercules.lua
@@ -1,15 +1,12 @@
 #!/usr/bin/env lua
 
--- shitface.lua
 local Pipeline = require("pipeline")
 
--- Ensure a file is provided
 if #arg < 1 then
     print("Usage: ./hercules <file.lua>")
     os.exit(1)
 end
 
--- Read the input file
 local input_file = arg[1]
 local file = io.open(input_file, "r")
 if not file then
@@ -20,10 +17,8 @@ end
 local code = file:read("*all")
 file:close()
 
--- Obfuscate the code
 local obfuscated_code = Pipeline.process(code)
 
--- Write the obfuscated code to an output file
 local output_file = input_file:gsub("%.lua$", "_obfuscated.lua")
 file = io.open(output_file, "w")
 file:write(obfuscated_code)
