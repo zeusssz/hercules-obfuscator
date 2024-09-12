00281a6cc9759884df1fd1b762a8f4c058ce3aab
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-12 12:41:10 +0530

    we do the funny

--- src/modules/watermark.lua
+++ src/modules/watermark.lua
@@ -1,8 +1,17 @@
 -- modules/watermark.lua
 local Watermark = {}
 
+local function compressLuaCode(code)
+    local compressedCode = code:gsub("%-%-[^\n]*", "")
+    compressedCode = compressedCode:gsub("%-%-%[(.-)%]%]", "")
+    compressedCode = compressedCode:gsub("%s+", " ")
+    compressedCode = compressedCode:gsub("([%S])\n([%S])", "%1 %2")
+    return compressedCode:match("^%s*(.-)%s*$")
+end
+
 function Watermark.add_watermark(code)
-    return "--[Obfuscated by Hercules]\n" .. code
+    local compressedCode = compressLuaCode(code)
+    return "--[Obfuscated by Hercules]\n" .. compressedCode
 end
 
 return Watermark
