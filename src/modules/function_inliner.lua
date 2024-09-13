8786054ef4f2d01c0261ebe4f6fa887807a4ed63
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-13 11:05:33 +0530

    patch

--- src/modules/function_inliner.lua
+++ src/modules/function_inliner.lua
@@ -4,21 +4,35 @@ function FunctionInliner.process(code)
     local functions = {}
 
     code = code:gsub("function%s+([%w_]+)%s*%((.-)%)%s*(.-)%s*end", function(func_name, params, func_body)
-        functions[func_name] = func_body
+        functions[func_name] = { body = func_body, params = params }
         return ""
     end)
 
     code = code:gsub("([%w_]+)%((.-)%)", function(func_name, args)
-        local func_body = functions[func_name]
-        if func_body then
-            func_body = func_body:gsub("%b()", function(param_block)
-                return args
-            end)
-            return func_body
+        local func = functions[func_name]
+        if func then
+            local inlined_body = func.body
+            local param_names = {}
+            for param in func.params:gmatch("[^,%s]+") do
+                table.insert(param_names, param)
+            end
+
+            local arg_values = {}
+            for arg in args:gmatch("[^,%s]+") do
+                table.insert(arg_values, arg)
+            end
+
+            for i = 1, #param_names do
+                local param = param_names[i]
+                local arg = arg_values[i] or ""
+                inlined_body = inlined_body:gsub("%f[%w_]" .. param .. "%f[^%w_]", arg)
+            end
+
+            return inlined_body
         end
         return func_name .. "(" .. args .. ")"
     end)
-    
+
     return code
 end
 
