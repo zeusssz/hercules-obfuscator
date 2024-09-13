5975d67f4f69e211b0c18b1909a1675ed26bef12
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-13 11:01:11 +0530

    patch

--- src/modules/bytecode_encoder.lua
+++ src/modules/bytecode_encoder.lua
@@ -9,16 +9,23 @@ function BytecodeEncoder.process(code)
         return string.format("loadstring(%q)()", string.dump(func))
     end
 
-    local encoded_code, gsub_error = code:gsub("(.-);", function(block)
+    local encoded_code, position = "", 1
+
+    while position <= #code do
+        local next_position = code:find(";", position)
+        if not next_position then
+            next_position = #code + 1
+        end
+        
+        local block = code:sub(position, next_position - 1)
         local success, result = pcall(encode_to_bytecode, block)
+        
         if not success then
             error("Error encoding block: " .. result)
         end
-        return result .. ";"
-    end)
-
-    if not encoded_code then
-        error("Failed to encode code: " .. gsub_error)
+        
+        encoded_code = encoded_code .. result .. ";"
+        position = next_position + 1
     end
 
     return encoded_code
