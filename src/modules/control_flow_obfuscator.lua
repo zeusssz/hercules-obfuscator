8794ea89a0fa49d43db82ca3bd0d1a360aa3d7da
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-13 11:04:43 +0530

    patch

--- src/modules/control_flow_obfuscator.lua
+++ src/modules/control_flow_obfuscator.lua
@@ -3,13 +3,20 @@ local ControlFlowObfuscator = {}
 function ControlFlowObfuscator.process(code)
     local function insert_fake_control_flow(original_code)
         local obfuscated_code = string.format(
-            "while true do if false then break end %s break end",
+            "while true do " ..
+            "if math.random(0, 1) == 0 then " ..
+            "local _ = %d " ..
+            "else break end " ..
+            "%s " ..
+            "if false then break end " ..
+            "end",
+            math.random(1, 1000),
             original_code
         )
         return obfuscated_code
     end
-    local obfuscated_code = insert_fake_control_flow(code)
 
+    local obfuscated_code = insert_fake_control_flow(code)
     return obfuscated_code
 end
 
