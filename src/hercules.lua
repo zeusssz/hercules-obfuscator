21079c8ef12fc52b0e546fcb8f6d18d05b83788e
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-15 16:18:42 +0530

    pls work

--- src/hercules.lua
+++ src/hercules.lua
@@ -2,12 +2,24 @@
 
 local Pipeline = require("pipeline")
 
-if #arg < 1 then
-    print("Usage: ./hercules <file.lua>")
+local function print_usage()
+    print("Usage: ./hercules <file.lua> [--overwrite]")
     os.exit(1)
 end
 
+if #arg < 1 then
+    print_usage()
+end
+
 local input_file = arg[1]
+local overwrite = false
+
+if #arg > 1 and arg[2] == "--overwrite" then
+    overwrite = true
+elseif #arg > 1 then
+    print_usage()
+end
+
 local file = io.open(input_file, "r")
 if not file then
     print("Error: Could not open file " .. input_file)
@@ -19,7 +31,13 @@ file:close()
 
 local obfuscated_code = Pipeline.process(code)
 
-local output_file = input_file:gsub("%.lua$", "_obfuscated.lua")
+local output_file
+if overwrite then
+    output_file = input_file
+else
+    output_file = input_file:gsub("%.lua$", "_obfuscated.lua")
+end
+
 file = io.open(output_file, "w")
 file:write(obfuscated_code)
 file:close()
