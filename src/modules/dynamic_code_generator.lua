60bfefc7a8b055ff8d9dd6754ecccbcc23bff1b1
zeus <136583387+zeusssz@users.noreply.github.com>
2024-10-10 10:57:25 +0530

    temp fix. again

--- src/modules/dynamic_code_generator.lua
+++ src/modules/dynamic_code_generator.lua
@@ -2,7 +2,7 @@ local DynamicCodeGenerator = {}
 
 function DynamicCodeGenerator.process(code)
     local function dynamic_wrapper(block)
-        return string.format("load(%q)()", block)
+        return load(block)()
     end
 
     local processed_code = ""
@@ -13,14 +13,14 @@ function DynamicCodeGenerator.process(code)
         if not next_position then
             next_position = #code + 1
         end
-        local block = code:sub(position, next_position - 1)
+        local block = code:sub(position, next_position - 1):gsub("\n", "")
 
         if #block > 0 then
             local success, result = pcall(dynamic_wrapper, block)
             if not success then
                 error("Error generating dynamic code: " .. result)
             end
-            processed_code = processed_code .. result
+            processed_code = processed_code .. result .. "\n"
         end
         position = next_position + 1
     end
