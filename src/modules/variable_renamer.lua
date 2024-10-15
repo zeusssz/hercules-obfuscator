3e2fb06497237c0058f5e4c60e2816319fecf390
zeus <136583387+zeusssz@users.noreply.github.com>
2024-10-15 20:10:40 +0530

    holy fuck thats a lot of whitelisted

--- src/modules/variable_renamer.lua
+++ src/modules/variable_renamer.lua
@@ -14,8 +14,38 @@ local lua_keywords = {
     ["type"] = true, ["print"] = true, ["string"] = true,
     ["table"] = true, ["math"] = true, ["os"] = true, ["coroutine"] = true,
     ["debug"] = true, ["io"] = true, ["utf8"] = true, ["bit32"] = true,
-    ["_G"] = true, ["_VERSION"] = true, ["write"] = true, ["sort"] = true, ["remove"] = true
+    ["_G"] = true, ["_VERSION"] = true, ["write"] = true, ["sort"] = true,
+    ["remove"] = true, ["math.abs"] = true, ["math.acos"] = true,
+    ["math.asin"] = true, ["math.atan"] = true, ["math.atan2"] = true,
+    ["math.ceil"] = true, ["math.cos"] = true, ["math.cosh"] = true,
+    ["math.deg"] = true, ["math.exp"] = true, ["math.floor"] = true,
+    ["math.fmod"] = true, ["math.frexp"] = true, ["math.ldexp"] = true,
+    ["math.log"] = true, ["math.log10"] = true, ["math.max"] = true,
+    ["math.min"] = true, ["math.modf"] = true, ["math.pi"] = true,
+    ["math.pow"] = true, ["math.rad"] = true, ["math.random"] = true,
+    ["math.randomseed"] = true, ["math.sin"] = true, ["math.sinh"] = true,
+    ["math.sqrt"] = true, ["math.tan"] = true, ["math.tanh"] = true,
+    ["string.byte"] = true, ["string.char"] = true, ["string.dump"] = true,
+    ["string.find"] = true, ["string.format"] = true, ["string.gmatch"] = true,
+    ["string.gsub"] = true, ["string.len"] = true, ["string.lower"] = true,
+    ["string.match"] = true, ["string.rep"] = true, ["string.reverse"] = true,
+    ["string.sub"] = true, ["string.upper"] = true,
+    ["table.concat"] = true, ["table.insert"] = true, ["table.remove"] = true,
+    ["table.sort"] = true, ["table.pack"] = true, ["table.unpack"] = true,
+    ["coroutine.create"] = true, ["coroutine.resume"] = true,
+    ["coroutine.yield"] = true, ["coroutine.running"] = true,
+    ["coroutine.status"] = true, ["coroutine.wrap"] = true,
+    ["debug.debug"] = true, ["debug.getinfo"] = true, ["debug.getregistry"] = true,
+    ["debug.getupvalue"] = true, ["debug.getuservalue"] = true,
+    ["debug.setfenv"] = true, ["debug.setmetatable"] = true,
+    ["debug.setuservalue"] = true, ["io.close"] = true, ["io.flush"] = true,
+    ["io.input"] = true, ["io.lines"] = true, ["io.open"] = true,
+    ["io.output"] = true, ["io.popen"] = true, ["io.read"] = true,
+    ["io.tmpfile"] = true, ["io.type"] = true, ["io.write"] = true,
+    ["utf8.char"] = true, ["utf8.charbyte"] = true, ["utf8.codepoint"] = true,
+    ["utf8.codes"] = true, ["utf8.len"] = true, ["utf8.offset"] = true
 }
+
 
 local function generate_random_name(len)
     len = len or math.random(8, 12)
