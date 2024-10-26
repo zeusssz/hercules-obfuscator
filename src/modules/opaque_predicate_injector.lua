47130532f72c61d30335075e2fc3c3276fc806d5
zeus <136583387+zeusssz@users.noreply.github.com>
2024-10-26 09:54:17 +0530

    Update opaque_predicate_injector.lua

--- src/modules/opaque_predicate_injector.lua
+++ src/modules/opaque_predicate_injector.lua
@@ -1,20 +1,19 @@
 local OpaquePredicateInjector = {}
 
 local predicates = {
-    "if (math.sin(math.pi) == 0) then ",
-    "if (3 & 1 == 1) then ",
-    "if (5^2 - 2^5 == 9) then ",
-    "if ((10 > 5 and 2 < 3) or (7 == 7)) then ",
-    "if (#('abc' .. 'def') == 6) then ",
+    " if (math.sin(math.pi) ~= 0) then ",
+    " if (5^2 - 2^5 == -7) then ",
+    " if ((10 > 5 and 2 < 3) or (7 == 7)) then ",
+    " if (#('abc' .. 'def') == 6) then ",
 }
 
 local function inject_predicate(block)
     local predicate = predicates[math.random(#predicates)]
-    return predicate .. block .. " end"
+    return predicate .. block .. " end;"
 end
 
 function OpaquePredicateInjector.process(code)
-    local processed_code = code:gsub("([ \t]*)(local%s+[^\n;]*;)", function(ws, var_def)
+    local processed_code = code:gsub("([ \t]*)([^\n;]*;)", function(ws, var_def)
         return ws .. inject_predicate(var_def)
     end)
 
