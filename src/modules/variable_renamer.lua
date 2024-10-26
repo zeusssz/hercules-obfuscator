d98e3970e018f7cd5e1356116cf77d23338efa0b
zeus <136583387+zeusssz@users.noreply.github.com>
2024-10-26 10:10:09 +0530

    Update variable_renamer.lua

--- src/modules/variable_renamer.lua
+++ src/modules/variable_renamer.lua
@@ -1,51 +1,24 @@
+--modules/variable_renamer.lua
 local VariableRenamer = {}
+local varencNames = {}
 
-local lua_keywords = {
-    ["and"] = true, ["break"] = true, ["do"] = true, ["else"] = true,
-    ["elseif"] = true, ["end"] = true, ["false"] = true, ["for"] = true,
-    ["function"] = true, ["goto"] = true, ["if"] = true, ["in"] = true,
-    ["local"] = true, ["nil"] = true, ["not"] = true, ["or"] = true,
-    ["repeat"] = true, ["return"] = true, ["then"] = true, ["true"] = true,
-    ["until"] = true, ["while"] = true, ["require"] = true,
-    ["module"] = true, ["package"] = true, ["self"] = true,
-    ["assert"] = true, ["collectgarbage"] = true, ["dofile"] = true,
-    ["loadfile"] = true, ["loadstring"] = true, ["pairs"] = true,
-    ["ipairs"] = true, ["tonumber"] = true, ["tostring"] = true,
-    ["type"] = true, ["print"] = true, ["string"] = true,
-    ["table"] = true, ["math"] = true, ["os"] = true, ["coroutine"] = true,
-    ["debug"] = true, ["io"] = true, ["utf8"] = true, ["bit32"] = true,
-    ["_G"] = true, ["_VERSION"] = true, ["write"] = true, ["sort"] = true,
-    ["remove"] = true, ["math.abs"] = true, ["math.acos"] = true,
-    ["math.asin"] = true, ["math.atan"] = true, ["math.atan2"] = true,
-    ["math.ceil"] = true, ["math.cos"] = true, ["math.cosh"] = true,
-    ["math.deg"] = true, ["math.exp"] = true, ["math.floor"] = true,
-    ["math.fmod"] = true, ["math.frexp"] = true, ["math.ldexp"] = true,
-    ["math.log"] = true, ["math.log10"] = true, ["math.max"] = true,
-    ["math.min"] = true, ["math.modf"] = true, ["math.pi"] = true,
-    ["math.pow"] = true, ["math.rad"] = true, ["math.random"] = true,
-    ["math.randomseed"] = true, ["math.sin"] = true, ["math.sinh"] = true,
-    ["math.sqrt"] = true, ["math.tan"] = true, ["math.tanh"] = true,
-    ["string.byte"] = true, ["string.char"] = true, ["string.dump"] = true,
-    ["string.find"] = true, ["string.format"] = true, ["string.gmatch"] = true,
-    ["string.gsub"] = true, ["string.len"] = true, ["string.lower"] = true,
-    ["string.match"] = true, ["string.rep"] = true, ["string.reverse"] = true,
-    ["string.sub"] = true, ["string.upper"] = true,
-    ["table.concat"] = true, ["table.insert"] = true, ["table.remove"] = true,
-    ["table.sort"] = true, ["table.pack"] = true, ["table.unpack"] = true,
-    ["coroutine.create"] = true, ["coroutine.resume"] = true,
-    ["coroutine.yield"] = true, ["coroutine.running"] = true,
-    ["coroutine.status"] = true, ["coroutine.wrap"] = true,
-    ["debug.debug"] = true, ["debug.getinfo"] = true, ["debug.getregistry"] = true,
-    ["debug.getupvalue"] = true, ["debug.getuservalue"] = true,
-    ["debug.setfenv"] = true, ["debug.setmetatable"] = true,
-    ["debug.setuservalue"] = true, ["io.close"] = true, ["io.flush"] = true,
-    ["io.input"] = true, ["io.lines"] = true, ["io.open"] = true,
-    ["io.output"] = true, ["io.popen"] = true, ["io.read"] = true,
-    ["io.tmpfile"] = true, ["io.type"] = true, ["io.write"] = true,
-    ["utf8.char"] = true, ["utf8.charbyte"] = true, ["utf8.codepoint"] = true,
-    ["utf8.codes"] = true, ["utf8.len"] = true, ["utf8.offset"] = true, ["insert"] = true
+local lua_functions = {
+    "assert", "collectgarbage", "dofile", "loadfile", "loadstring",
+    "pairs", "ipairs", "tonumber", "tostring", "type", "print",
+    "_G", "_VERSION", "write", "sort",
+    "math.abs", "math.acos", "math.asin", "math.atan", "math.atan2",
+    "math.ceil", "math.cos", "math.cosh", "math.deg", "math.exp",
+    "math.floor", "math.fmod", "math.frexp", "math.ldexp", "math.log",
+    "math.log10", "math.max", "math.min", "math.modf", "math.pi",
+    "math.pow", "math.rad", "math.random", "math.randomseed", "math.sin",
+    "math.sinh", "math.sqrt", "math.tan", "math.tanh",
+    "string.byte", "string.char", "string.dump", "string.find",
+    "string.format", "string.gmatch", "string.gsub", "string.len",
+    "string.lower", "string.match", "string.rep", "string.reverse",
+    "string.sub", "string.upper",
+    "table.concat", "table.insert", "table.remove", "table.sort",
+    "table.pack", "table.unpack"
 }
-
 
 local function generate_random_name(len)
     len = len or math.random(8, 12)
@@ -58,53 +31,64 @@ local function generate_random_name(len)
     return name
 end
 
-function VariableRenamer.process(code)
-    local variables = {}
-    local result = {}
-    local in_string = false
-    local current_quote
-    local last_position = 1
-
-    for i = 1, #code do
-        local char = code:sub(i, i)
-
-        if not in_string then
-            if char == "'" or char == '"' then
-                if last_position < i then
-                    local segment = code:sub(last_position, i - 1)
-                    segment = segment:gsub("([%a_][%w_]*)", function(var)
-                        if not lua_keywords[var] and not variables[var] then
-                            variables[var] = generate_random_name()
-                        end
-                        return variables[var] or var
-                    end)
-                    table.insert(result, segment)
-                end
-                in_string = true
-                current_quote = char
-                table.insert(result, char)
-                last_position = i + 1
-            end
-        else
-            if char == current_quote and (i == 1 or code:sub(i - 1, i - 1) ~= "\\") then
-                table.insert(result, code:sub(last_position, i))
-                in_string = false
-                last_position = i + 1
+local function obfuscate_local_variables(code)
+    local local_var_pattern = "local%s+([%w_,%s]+)%s*="
+    local local_func_pattern = "local%s+function%s+([%w_]+)%s*%(([%w_,%s]*)%)"
+    local obfuscated_code = code
+    local local_var_map = {}
+    for local_vars in code:gmatch(local_var_pattern) do
+        for var in local_vars:gmatch("[%w_]+") do
+            if not local_var_map[var] then
+                local_var_map[var] = generate_random_name()
             end
         end
     end
-    if last_position <= #code then
-        local segment = code:sub(last_position)
-        segment = segment:gsub("([%a_][%w_]*)", function(var)
-            if not lua_keywords[var] and not variables[var] then
-                variables[var] = generate_random_name()
+
+    for vars, args in code:gmatch(local_func_pattern) do
+        if not local_var_map[vars] then
+            local_var_map[vars] = generate_random_name()
+        end
+        for arg in args:gmatch("[^,]+") do
+            if not local_var_map[arg:match("^%s*(.-)%s*$")] then
+                local_var_map[arg:match("^%s*(.-)%s*$")] = generate_random_name()
             end
-            return variables[var] or var
-        end)
-        table.insert(result, segment)
+        end
     end
 
-    return table.concat(result)
+    obfuscated_code = obfuscated_code:gsub(local_var_pattern, function(local_vars)
+        return "local " .. local_vars:gsub("[%w_]+", function(var)
+            return local_var_map[var] or var
+        end) .. '='
+    end)
+
+    for original_var, obfuscated_var in pairs(local_var_map) do
+        obfuscated_code = obfuscated_code:gsub("(%W)(" .. original_var .. ")(%W)", function(pre, var, post)
+            return pre .. obfuscated_var .. post
+        end)
+    end
+
+    return obfuscated_code
+end
+
+function VariableRenamer.process(code)
+    code = obfuscate_local_variables(code)
+
+    local renamed_vars = {}
+    local assignment_lines = {}
+    local replacements = code
+    for _, function_name in ipairs(lua_functions) do
+        if string.find(code, function_name, 1, true) then
+            if not varencNames[function_name] then
+                local new_name = generate_random_name()
+                varencNames[function_name] = new_name
+                table.insert(renamed_vars, new_name)
+                table.insert(assignment_lines, new_name .. " = " .. function_name .. ";")
+            end
+            replacements = string.gsub(replacements, function_name, varencNames[function_name])
+        end
+    end
+    local local_declaration = "local " .. table.concat(renamed_vars, ", ")
+    return local_declaration .. "\n" .. table.concat(assignment_lines, " ") .. "\n" .. replacements
 end
 
 return VariableRenamer
