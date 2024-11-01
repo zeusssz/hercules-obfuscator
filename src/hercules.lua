dc0c0b36e1202b143e9a9cb1a4dffbcfeb6ea20c
zeus <136583387+zeusssz@users.noreply.github.com>
2024-11-01 19:55:10 +0530

    Update hercules.lua

--- src/hercules.lua
+++ src/hercules.lua
@@ -69,7 +69,6 @@ local function print_result(input, output, time, overwrite, custom_file, sanity_
         { "Dynamic Code", config.get("settings.dynamic_code.enabled") },
         { "Bytecode Encoding", config.get("settings.bytecode_encoding.enabled") },
         { "Compressor", config.get("settings.compressor.enabled") },
-        { "Watermark", config.get("settings.watermark_enabled") },
     }
 
     local max_length = 0
