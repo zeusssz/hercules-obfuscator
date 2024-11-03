c31e0a06fded75ee02db38274de4a825c311bfb5
zeus <136583387+zeusssz@users.noreply.github.com>
2024-11-03 21:05:14 +0530

    Update variable_renamer.lua

--- src/modules/variable_renamer.lua
+++ src/modules/variable_renamer.lua
@@ -16,7 +16,8 @@ local lua_functions = {
     "string.lower", "string.match", "string.rep", "string.reverse",
     "string.sub", "string.upper",
     "table.concat", "table.insert", "table.remove", "table.sort",
-    "table.pack", "table.unpack", "game.Workspace"
+    "table.pack", "table.unpack", "game.Workspace", "game:GetService", "FireServer",
+    
 }
 
 local function generate_random_name(len)
