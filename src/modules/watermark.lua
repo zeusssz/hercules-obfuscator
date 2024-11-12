4b48a3095f36ce8880c99392932752e10b7adb17
zeus <136583387+zeusssz@users.noreply.github.com>
2024-11-12 20:57:24 +0530

    yea

--- src/modules/watermark.lua
+++ src/modules/watermark.lua
@@ -2,7 +2,7 @@
 local Watermark = {}
 
 function Watermark.process(code)
-    return "--[Obfuscated by Hercules v1.6 | discord.gg/Hx6RuYs8Ku]\n" .. code
+    return "--[Obfuscated by Hercules v1.6 | discord.gg/Hx6RuYs8Ku (server deleted)]\n" .. code
 end
 
 return Watermark
