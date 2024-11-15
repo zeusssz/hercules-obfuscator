8b7d9606e89e089597fc322a1b655cf93f7cfdd6
zeus <136583387+zeusssz@users.noreply.github.com>
2024-11-15 11:19:35 +0530

    Update StringToExpressions.lua

--- src/modules/StringToExpressions.lua
+++ src/modules/StringToExpressions.lua
@@ -1,13 +1,18 @@
-local stringtoexpressionreall = {}
+local stringtoexpressions = {}
 local math_methods = {
     add_sub = function(char, base1, base2)
         local base = math.random(base1, base2)
-        local chance = math.random(0,1)
-        local add_sub_expression = chance == 1 and string.format("%d - %d", base, base - char) or chance == 0 and string.format("%d + %d", char - base, base)
+        local chance = math.random(0, 1)
+        local add_sub_expression = chance == 1 and string.format("%d - %d", base, base - char) or string.format("%d + %d", char - base, base)
         return add_sub_expression
     end,
 }
+
 local function obfuscate_string_literal(str, base1, base2)
+    if #str == 0 then
+        return '""' 
+    end
+
     local obfuscated = {}
     for i = 1, #str do
         local char = str:byte(i)
@@ -16,9 +21,11 @@ local function obfuscate_string_literal(str, base1, base2)
     end
     return table.concat(obfuscated, "..")
 end
-function stringtoexpressionreall.process(script_content, base1, base2)
+
+function stringtoexpressions.process(script_content, base1, base2)
     return script_content:gsub("(['\"])(.-)%1", function(_, str)
         return obfuscate_string_literal(str, base1, base2)
     end)
 end
-return stringtoexpressionreall
+
+return stringtoexpressions
