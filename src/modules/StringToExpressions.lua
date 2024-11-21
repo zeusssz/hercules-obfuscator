dca95f3c23c721d3924f4160120c4e3f43a5a1ff
iwarp <20696463+iwarp@users.noreply.github.com>
2024-11-22 09:07:17 +1300

    support control codes in the strings

--- src/modules/StringToExpressions.lua
+++ src/modules/StringToExpressions.lua
@@ -8,17 +8,47 @@ local math_methods = {
     end,
 }
 
+local function insert_char(obfuscated, ascii_code, base1, base2)
+    local part = math_methods.add_sub(ascii_code, base1, base2)
+    table.insert(obfuscated, "string.char(" .. part .. ")")
+end
+
 local function obfuscate_string_literal(str, base1, base2)
     if #str == 0 then
         return '""' 
     end
 
     local obfuscated = {}
-    for i = 1, #str do
-        local char = str:byte(i)
-        local part = math_methods.add_sub(char, base1, base2)
-        table.insert(obfuscated, "string.char(" .. part .. ")")
+    local i = 1
+
+    while i <= #str do
+        local char = str:sub(i, i)
+
+        -- look for char starting with \
+        if char == "\\" and i < #str then
+            local next_char = str:sub(i + 1, i + 1)
+
+            if next_char == "n" then
+               insert_char(obfuscated, 10, base1, base2) -- ASCII code for \n
+            elseif next_char == "r" then
+                insert_char(obfuscated, 13, base1, base2) -- ASCII code for \r
+            elseif next_char == "t" then
+                insert_char(obfuscated, 9, base1, base2) -- ASCII code for \t
+            else
+                -- remove the escaping and just insert the next_char
+                insert_char(obfuscated, next_char:byte(), base1, base2)
+            end
+            
+            -- skip ahead
+            i = i + 1
+        else
+            -- Handle regular characters
+            insert_char(obfuscated, char:byte(), base1, base2)
+        end
+        
+        i = i + 1
     end
+
     return table.concat(obfuscated, "..")
 end
 
