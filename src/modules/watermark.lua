53fccb717a82ad89d327fdf923bcbfbb6d466409
zeus <136583387+zeusssz@users.noreply.github.com>
2025-02-04 21:03:43 +0530

    feat: add placeholder for discord server. soon....

--- src/modules/watermark.lua
+++ src/modules/watermark.lua
@@ -2,7 +2,7 @@
 local Watermark = {}
 
 function Watermark.process(code)
-    return "--[Obfuscated by Hercules v1.6.2 | github.com/zeusssz/hercules-obfuscator]\n" .. code
+    return "--[Obfuscated by Hercules v1.6.2 | github.com/zeusssz/hercules-obfuscator | .gg/placeholder]\n" .. code
 end
 
 return Watermark
