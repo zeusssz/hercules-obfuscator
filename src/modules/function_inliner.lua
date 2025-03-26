d1dd6d5a49ddbe3fcc4252158637a042bb61e63d
playvora <longliator.dev@gmail.com>
2025-03-26 13:59:20 +0700

    fixed function inliner

--- src/modules/function_inliner.lua
+++ src/modules/function_inliner.lua
@@ -1,41 +1,61 @@
 local FunctionInliner = {}
 
 function FunctionInliner.process(code)
-    local functions = {}
-    code = code:gsub("function%s+([%w_]+)%s*%((.-)%)%s*(.-)%s*end", function(func_name, params, func_body)
-        functions[func_name] = { body = func_body, params = params }
-        return ""
-    end)
-
-    code = code:gsub("local%s+function%s+([%w_]+)%s*%((.-)%)%s*(.-)%s*end", function(func_name, params, func_body)
-        return "local function " .. func_name .. "(" .. params .. ")" .. func_body .. " end"
-    end)
-    code = code:gsub("([%w_]+)%s*%((.-)%)", function(func_name, args)
-        local func = functions[func_name]
-        if func then
-            local inlined_body = func.body
-            local param_names = {}
-            for param in func.params:gmatch("[^,%s]+") do
-                table.insert(param_names, param)
-            end
-
-            local arg_values = {}
-            for arg in args:gmatch("[^,]+") do
-                table.insert(arg_values, arg:match("^%s*(.-)%s*$"))
-            end
-            for i = 1, #param_names do
-                local param = param_names[i]
-                local arg = arg_values[i] or "nil"
-                inlined_body = inlined_body:gsub("%f[%w_]" .. param .. "%f[^%w_]", "(" .. arg .. ")")
-            end
-            inlined_body = inlined_body:gsub("return%s+(.+)", "%1")
-            
-            return "(" .. inlined_body .. ")"
-        end
-        return func_name .. "(" .. args .. ")"
-    end)
-
+  if code:match("^%s*%-%-.*Obfuscated") then
     return code
+  end
+
+  local functions = {}
+  
+  code = code:gsub("local%s+function%s+([%w_]+)%s*%((.-)%)(.-)end", function(name, params, body)
+    if body:sub(1,1) ~= "\n" then return end
+    
+    body = body:sub(2)
+    functions[name] = {params = params, body = body}
+    return ""
+  end)
+  
+  code = code:gsub("function%s+([%w_]+)%s*%((.-)%)(.-)end", function(name, params, body)
+    if body:sub(1,1) ~= "\n" then return end
+    
+    body = body:sub(2)
+    functions[name] = {params = params, body = body}
+    return ""
+  end)
+  
+  code = code:gsub("([%w_]+)%s*%((.-)%)", function(name, args)
+    if not functions[name] then
+      return name .. "(" .. args .. ")"
+    end
+    
+    local func = functions[name]
+    local body = func.body
+    
+    if func.params == "..." then
+      body = body:gsub("%.%.%.", args)
+    else
+      local params = {}
+      for param in func.params:gmatch("[^%s,]+") do
+        table.insert(params, param)
+      end
+      
+      local arguments = {}
+      for arg in args:gmatch("[^,]+") do
+        table.insert(arguments, arg:match("^%s*(.-)%s*$"))
+      end
+      
+      for i = 1, #params do
+        local param = params[i]
+        local arg = arguments[i] or "nil"
+        body = body:gsub("%f[%w_]" .. param .. "%f[^%w_]", arg)
+      end
+    end
+    
+    body = body:gsub("return%s+", "")
+    return body:match("^%s*(.-)%s*$")
+  end)
+  
+  return code
 end
 
 return FunctionInliner
