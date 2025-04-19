32d71463d6adb274f2b67332abfd9c99848976a7
zeus <136583387+zeusssz@users.noreply.github.com>
2025-04-19 08:23:11 +0530

    feat: debug.getinfo thing

--- src/modules/antitamper.lua
+++ src/modules/antitamper.lua
@@ -2,8 +2,20 @@ local AntiTamper = {}
 -- only anti beautify for the time being. wait for proper anti tamper.
 function AntiTamper.process(code)
   local antiBeautifyCode = [[
+local __debug = debug
+local __getinfo = __debug.getinfo
+
+if type(__getinfo) ~= "function" or tostring(__getinfo):sub(1, 8) ~= "function" then
+  print("HERCULES: Tampering Detected!")
+  return
+end
+
 local function __antiBeautifyCheck()
-  local info = debug.getinfo(2, "nSl")
+  if debug.getinfo ~= __getinfo then
+    print("HERCULES: debug.getinfo override detected!")
+    return true
+  end
+  local info = __getinfo(2, "nSl")
   if not info or info.currentline ~= 2 or info.linedefined ~= 2 then
     print("HERCULES: Beautification Detected!")
     return true
