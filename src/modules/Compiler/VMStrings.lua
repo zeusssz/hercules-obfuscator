4be1c1e1917e74f1003d9cba1839ef6db53f3bb2
playvora <longliator.dev@gmail.com>
2025-04-21 08:08:29 -0700

    fixed this thing

--- src/modules/Compiler/VMStrings.lua
+++ src/modules/Compiler/VMStrings.lua
@@ -107,7 +107,7 @@ function BcToState(Bytecode, charset)
     local base, decoded = #charset, {}
     local decode_lookup = {}
     for i = 1, base do decode_lookup[charset:sub(i, i)] = i - 1 end
-    for encoded_char in Bytecode:gmatch("[^\0]+") do
+    for encoded_char in (Bytecode .. "\0"):gmatch("([^%z]*)%z") do
         local n = 0
         for i = 1, #encoded_char do n = n * base + decode_lookup[encoded_char:sub(i, i)] end
         decoded[#decoded + 1] = string.char(n)
