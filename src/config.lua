1b2051094ea0130b9eac3b64695caf9b29340942
Serpensin <wissmann.matti@gmail.com>
2026-04-30 19:25:07 +0200

    fix: enable StringToExpressions, string_encoding, function_inlining, dynamic_code, and bytecode_encoding modules

--- src/config.lua
+++ src/config.lua
@@ -17,12 +17,12 @@ config.settings = {
         max_fake_blocks = 6,
     },
     StringToExpressions = {
-        enabled = false,
+        enabled = true,
         min_number_length = 100,
         max_number_length = 999,
     },
     string_encoding = {
-        enabled = false,
+        enabled = true,
     },
     WrapInFunction = {
         enabled = true,
@@ -40,13 +40,13 @@ config.settings = {
         enabled = true,
     },
     function_inlining = {
-        enabled = false,
+        enabled = true,
     },
     dynamic_code = {
-        enabled = false,
+        enabled = true,
     },
     bytecode_encoding = {
-        enabled = false,
+        enabled = true,
     },
     compressor = {
         enabled = true,
